eab07959a44e4ac734b2f6527c26731d127dc27c
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-01-24 19:28:40 +0100

    first commit

---
+++ fotoserver/src/main/java/com/xdteam/fotoserver/FotoserverApplication.java
@@ -0,0 +1,13 @@
+package com.xdteam.fotoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoserverApplication.class, args);
+	}
+
+}
