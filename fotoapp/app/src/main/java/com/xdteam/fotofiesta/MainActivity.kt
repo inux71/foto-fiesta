4016ab4b0b6bf95530242cd2714026a954cbbba4
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2023-01-26 20:19:18 +0100

    feat: setup dependency injection 💉

--- fotoapp/app/src/main/java/com/xdteam/fotofiesta/MainActivity.kt
+++ fotoapp/app/src/main/java/com/xdteam/fotofiesta/MainActivity.kt
@@ -3,6 +3,7 @@ package com.xdteam.fotofiesta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.xdteam.fotofiesta.ui.theme.FotoFiestaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
