8eb62d0edd0ca7d01a788bd8c0b644fe777ee0b2
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-02-20 11:25:08 +0100

    clear fotoserver code

--- fotoserver/src/main/java/com/xdteam/fotoserver/controller/StorageController.java
+++ fotoserver/src/main/java/com/xdteam/fotoserver/controller/StorageController.java
@@ -21,21 +21,30 @@ public class StorageController {
     private StorageService storageService;
 
     @PostMapping("/oneFile")
-    public ResponseEntity<String> uploadOneFile(@RequestParam("file") MultipartFile file, @RequestParam("seriesId") String seriesId) {
+    public ResponseEntity<String> uploadOneFile(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("seriesId") String seriesId
+    ) {
         String fileName = storageService.storeOneFile(file, seriesId);
         String fileUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/file/").path(fileName).toUriString();
         return ResponseEntity.ok().body(fileUri);
     }
 
     @PostMapping("/files")
-    public ResponseEntity<String> uploadFiles(@RequestParam("files") List<MultipartFile> files, @RequestParam("seriesId") String seriesId) throws IOException {
+    public ResponseEntity<String> uploadFiles(
+            @RequestParam("files") List<MultipartFile> files,
+            @RequestParam("seriesId") String seriesId
+    ) throws IOException {
         String pdfName = storageService.storeMultipleFiles(files,seriesId);
         String pdfUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/pdf/").path(pdfName).toUriString();
         return ResponseEntity.ok().body(pdfUri);
     }
 
     @GetMapping("/pdf/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
+    public ResponseEntity<Resource> downloadFile(
+            @PathVariable String fileName,
+            HttpServletRequest request
+    ) throws IOException {
         org.springframework.core.io.Resource resource = storageService.loadFileAsResource(fileName);
         String contentType = null;
         try {
@@ -49,8 +58,10 @@ public class StorageController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\""
+                )
                 .body(resource);
     }
 }
-
